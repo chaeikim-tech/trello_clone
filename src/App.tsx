13a6a73e9e9c493ae11633055f9d5057f11fbdb0
eunju.kim <sb02348@hanmail.net>
2022-02-12 00:00:05 +0900

    Set Selector example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hourSelector, minuteState } from './atoms';
 
 function App() {
     const [ minutes, setMinutes ] = useRecoilState(minuteState);
     // useRecoilState는 atom의 값에 더해서 atom을 수정할 함수까지 준다.
-    const hours = useRecoilValue(hourSelector);
+    const [ hours, setHours ] = useRecoilState(hourSelector);
+    // setHours는 set property를 실행시키는 함수.
     const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
         setMinutes(+event.currentTarget.value);
         // 기본값이 number인데 input에 넘어오는 값은 string. '+' 붙여주면 숫자로 바꿔줌.
         // +"1" => 1 
     };
+    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
+    };
     return(
         <div>
             <input value={minutes}  onChange={onMinutesChange} type="number" placeholder="Minutes" />
-            <input value={hours} type="number" placeholder="Hours" />
+            <input onChange={onHoursChange} value={hours} type="number" placeholder="Hours" />
             {/* input에 onChangeEvent가 없고 값만 있다면 읽기만 가능. */}
         </div>
     )
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -5,11 +5,18 @@ export const minuteState = atom({
   default: 0,
 });
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key: "hours",
   get: ({ get }) => {
     const minutes = get(minuteState);
+    //get함수를 사용해서 atom의 값을 가져올 수 있음.
     return minutes / 60;
-    // selector를 활용해서 state를 가져오고 output 수정.
+    // selector를 활용해서 state를 가져오고 output 수정.    
+  },
+  set:({set}, newValue) => {
+    // set은 state를 set하는 것을 도와주는 속성.(atom수정하도록 도와줌)
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+    // 1. 수정하고 싶은 atom 2. 새로운 값. 
   },
 });
