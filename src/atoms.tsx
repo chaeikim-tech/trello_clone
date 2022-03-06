8df83a63bc6053f7f7c5419ec3a28f3cb726dba5
eunju.kim <sb02348@hanmail.net>
2022-03-07 02:46:42 +0900

    Reordering using onDragEnd

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,22 +1,6 @@
 import { atom, selector } from "recoil";
 
-export const minuteState = atom({
-  key: "minutes",
-  default: 0,
-});
-
-export const hourSelector = selector<number>({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minuteState);
-    //get함수를 사용해서 atom의 값을 가져올 수 있음.
-    return minutes / 60;
-    // selector를 활용해서 state를 가져오고 output 수정.    
-  },
-  set:({set}, newValue) => {
-    // set은 state를 set하는 것을 도와주는 속성.(atom수정하도록 도와줌)
-    const minutes = Number(newValue) * 60;
-    set(minuteState, minutes);
-    // 1. 수정하고 싶은 atom 2. 새로운 값. 
-  },
+export const toDoState  = atom({
+  key: "toDo",
+  default: ["a", "b", "c", "d", "e", "f"],
 });
